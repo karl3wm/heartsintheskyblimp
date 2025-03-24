05c7e5cb5fc93d72e80acef2614a094da1868609
karl <karl3wm@writeme.com>
2025-03-24 12:14:12 -0400

    -

---
+++ build_pigpio_termux.bash
@@ -0,0 +1,6 @@
+git submodule update --init --recursive --filter=blob:none arduinogpio/pigpio
+mkdir build_pigpio
+cd build_pigpio
+cmake ../arduinogpio/pigpio -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS='-Wno-deprecated-non-prototype -include strings.h -include netinet/in.h -Dpthread_cancel\(th\)=ENOSYS -DPTHREAD_CANCEL_DISABLE=1 -Dpthread_setcancelstate\(st,o_st\)=\(\(void\)\(st\),o_st?*\(int*\)\(o_st\)=PTHREAD_CANCEL_DISABLE:0,ENOSYS\)' &&
+make &&
+make install
