745f368088e1651c4057cf6c7b9a76c410e90949
Your Name <you@example.com>
2025-03-24 17:35:51 -0400

    run script for arduinogpiod

--- android-termux/build_pigpio_termux.bash
+++ android-termux/build_pigpio_termux.bash
@@ -4,3 +4,7 @@ cd build_pigpio
 cmake ../../arduinogpio/pigpio -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS='-Wno-deprecated-non-prototype -include strings.h -include netinet/in.h -Dpthread_cancel\(th\)=ENOSYS -DPTHREAD_CANCEL_DISABLE=1 -Dpthread_setcancelstate\(st,o_st\)=\(\(void\)\(st\),o_st?*\(int*\)\(o_st\)=PTHREAD_CANCEL_DISABLE:0,ENOSYS\)' &&
 make &&
 make install && cp -va "$PREFIX"/man/. -va "$PREFIX"/share/man/. && rm -rf "$PREFIX"/man
+
+echo ''
+echo "pigs h for help"
+echo ''
