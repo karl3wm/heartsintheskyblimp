6f6774e85ba65481cb2e3e52a6a9c5acedccc731
karl <karl3wm@writeme.com>
2025-03-24 12:26:57 -0400

    arduino folder

---
+++ android-termux/modified_cdc_example.c
@@ -0,0 +1,167 @@
+/*
+ * This is a simple example to communicate with a CDC-ACM USB device
+ * using libusb.
+ *
+ * Author: Christophe Augier <christophe.augier@gmail.com>
+ */
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+#include <libusb-1.0/libusb.h>
+
+#define ACM_CTRL_DTR   0x01
+#define ACM_CTRL_RTS   0x02
+
+/* We use a global variable to keep the device handle
+ */
+static struct libusb_device_handle *devh = NULL;
+
+/* The Endpoint address are hard coded. You should use lsusb -v to find
+ * the values corresponding to your device.
+ */
+static int ep_in_addr  = 0x83;
+static int ep_out_addr = 0x04; // UNO
+
+int write_char(unsigned char c)
+{
+    /* To send a char to the device simply initiate a bulk_transfer to the
+     * Endpoint with address ep_out_addr.
+     */
+    int actual_length;
+    int rc;
+    if ((rc = libusb_bulk_transfer(devh, ep_out_addr, &c, 1,
+                             &actual_length, 0)) < 0) {
+        fprintf(stderr, "Error while sending char: %s\n", libusb_error_name(rc));
+        return -1;
+    }
+    return 0;
+}
+
+int read_chars(unsigned char * data, int size)
+{
+    /* To receive characters from the device initiate a bulk_transfer to the
+     * Endpoint with address ep_in_addr.
+     */
+    int actual_length;
+    int rc = libusb_bulk_transfer(devh, ep_in_addr, data, size, &actual_length,
+                                  1000);
+    if (rc == LIBUSB_ERROR_TIMEOUT) {
+        //printf("timeout (%d)\n", actual_length);
+        return 0;
+    } else if (rc < 0) {
+        fprintf(stderr, "Error while waiting for char: %s\n", libusb_error_name(rc));
+        return -1;
+    }
+
+    return actual_length;
+}
+
+int main(int argc, char **argv)
+{
+    libusb_context * ctx;
+    int rc;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: termux-usb -e %s /dev/bus/usb/00?/00?\n", argv[0]);
+        exit(1);
+    }
+
+    /* Set debugging output to max level.
+     */
+    libusb_set_debug(NULL, 3);
+
+    /* Initialize libusb
+     */
+    libusb_set_option(NULL, LIBUSB_OPTION_NO_DEVICE_DISCOVERY);
+    rc = libusb_init(&ctx);
+    if (rc < 0) {
+        fprintf(stderr, "Error initializing libusb: %s\n", libusb_error_name(rc));
+        exit(1);
+    }
+
+    /* Look for a specific device and open it.
+     */
+    int fd;
+    rc = sscanf(argv[1], "%d", &fd);
+    if (rc != 1) {
+        fprintf(stderr, "Failed to read fd: %d\n", rc);
+        exit(1);
+    }
+    rc = libusb_wrap_sys_device(ctx, (intptr_t) fd, &devh);
+    if (rc != 0) {
+        fprintf(stderr, "Failed to wrap fd %d: %s\n", fd, libusb_error_name(rc));
+        exit(1);
+    }
+
+    for (int if_num = 0; if_num < 2; if_num++) {
+        if (libusb_kernel_driver_active(devh, if_num)) {
+            libusb_detach_kernel_driver(devh, if_num);
+        }
+        rc = libusb_claim_interface(devh, if_num);
+        if (rc < 0) {
+            fprintf(stderr, "Error claiming interface: %s\n",
+                    libusb_error_name(rc));
+            goto out;
+        }
+    }
+
+    /* Start configuring the device:
+     * - set line state
+     */
+    rc = libusb_control_transfer(devh, 0x21, 0x22, ACM_CTRL_DTR | ACM_CTRL_RTS,
+                                0, NULL, 0, 0);
+    if (rc < 0) {
+        fprintf(stderr, "Error during control transfer: %s\n",
+                libusb_error_name(rc));
+        goto out;
+    }
+
+    /* - set line encoding: here 9600 8N1
+     * 9600 = 0x2580 ~> 0x80, 0x25 in little endian
+     */
+    unsigned char encoding[] = { 0x80, 0x25, 0x00, 0x00, 0x00, 0x00, 0x08 };
+    rc = libusb_control_transfer(devh, 0x21, 0x20, 0, 0, encoding,
+                                sizeof(encoding), 0);
+    if (rc < 0) {
+        fprintf(stderr, "Error during control transfer: %s\n",
+                libusb_error_name(rc));
+        goto out;
+    }
+
+    /* We can now start sending or receiving data to the device
+     */
+    unsigned char buf[65];
+    int len, inputlen;
+
+    fcntl(0, F_SETFL, fcntl(0, F_GETFL) | O_NONBLOCK);
+
+    fprintf(stderr, "Connected.\n");
+
+    while(1) {
+        while ((inputlen = read(0, buf, 1)) == 1) {
+            if (write_char(buf[0])) {
+                break;
+            }
+        }
+        if (inputlen < 0 && errno != EAGAIN) {
+            perror("read");
+            break;
+        }
+        len = read_chars(buf, 64);
+        if (len == -1) { fprintf(stderr, "failed read chars\n"); break; }
+        if (len == 0) { usleep(150000); continue; }
+        buf[len] = 0;
+        fprintf(stdout, "%s", buf);
+        fflush(stdout);
+    }
+
+    libusb_release_interface(devh, 0);
+
+out:
+    if (devh)
+            libusb_close(devh);
+    libusb_exit(ctx);
+    return rc;
+}
