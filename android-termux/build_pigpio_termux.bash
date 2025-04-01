c39a63493683079b92075299e546dd16d3443497
Your Name <you@example.com>
2025-04-01 09:27:29 -0400

    -

--- android-termux/build_pigpio_termux.bash
+++ android-termux/build_pigpio_termux.bash
@@ -1,4 +1,4 @@
-git submodule update --init --recursive --filter=blob:none arduinogpio/pigpio
+git submodule update --init --recursive --filter=blob:none ../arduinogpio/pigpio
 mkdir build_pigpio
 cd build_pigpio
 cmake ../../arduinogpio/pigpio -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS='-Wno-deprecated-non-prototype -include strings.h -include netinet/in.h -Dpthread_cancel\(th\)=ENOSYS -DPTHREAD_CANCEL_DISABLE=1 -Dpthread_setcancelstate\(st,o_st\)=\(\(void\)\(st\),o_st?*\(int*\)\(o_st\)=PTHREAD_CANCEL_DISABLE:0,ENOSYS\)' &&
