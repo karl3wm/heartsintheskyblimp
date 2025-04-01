8f1d131cae451295b98cd0d93851d0c981ac4418
Your Name <you@example.com>
2025-04-01 09:41:28 -0400

    -

--- android-termux/build_modified_cdc_example.bash
+++
@@ -1,1 +0,0 @@
-cc -o modified_cdc_example modified_cdc_example.c -lusb-1.0 -O2 -std=c99 -Wall
--- android-termux/run_arduino_gpiod.bash
+++ android-termux/run_arduino_gpiod.bash
@@ -1,8 +1,8 @@
+echo Warning: This script currently accesses the first device listed by termux-usb -l . This could be the wrong device.
 DEV="$(termux-usb -l | jq -r .[0])"
 if ! termux-usb -e true "$DEV"
 then
 	echo "Requesting permissions for $DEV . Please confirm on device screen. This will then pause for a long time (insufficiently resolved termux bug)."
-	echo "Good time to run ./build_pigpio_termux.bash while waiting."
 	termux-usb -r "$DEV"
 fi
 echo starting
